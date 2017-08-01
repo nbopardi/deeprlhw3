6b7aab22333bf8ba797cbd043c92fc22d0706128
Nikhil <nikhilb@gmail.com>
2017-07-31 21:29:54 -0700

    changed screen render size and added wait for delay to slow down game speed

--- test_dqn_atari.py
+++ test_dqn_atari.py
@@ -8,10 +8,11 @@ import random
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
-
+import time
 import dqn
 from dqn_utils import *
 from atari_wrappers import *
+from gym.envs.classic_control import rendering
 
 # def atari_model(img_in, num_actions, scope, reuse=False):
 #     # as described in https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
@@ -66,6 +67,19 @@ def get_env(task, seed):
     env = wrap_deepmind(env)
 
     return env
+
+def repeat_upsample(rgb_array, k=1, l=1, err=[]):
+    # repeat kinda crashes if k/l are zero
+    if k <= 0 or l <= 0: 
+        if not err: 
+            print "Number of repeats must be larger than 0, k: {}, l: {}, returning default array!".format(k, l)
+            err.append('logged')
+        return rgb_array
+
+    # repeat the pixels k times along the y axis and l times along the x axis
+    # if the input image is of shape (m,n,3), the output image will be of shape (k*m, l*n, 3)
+
+    return np.repeat(np.repeat(rgb_array, k, axis=0), l, axis=1)
 
 def test_model(model_name, env, replay_buffer_size = 1000000, frame_history_len = 4):
     with tf.Session(graph = tf.Graph()) as sess:
@@ -123,6 +137,8 @@ def test_model(model_name, env, replay_buffer_size = 1000000, frame_history_len 
         mean_episode_reward      = -float('nan')
         best_mean_episode_reward = -float('inf')
         iteration = 0
+
+        viewer = rendering.SimpleImageViewer()
         while not done:
 
             obs_idx = replay_buffer.store_frame(last_obs)
@@ -134,7 +150,11 @@ def test_model(model_name, env, replay_buffer_size = 1000000, frame_history_len 
             action = sess.run([action_predict], feed_dict = {obs_t_ph: replay_obs})[0]
             last_obs, reward, done, info = env.step(action)
             
-            env.render()
+            time.sleep(0.03)
+            rgb = env.render('rgb_array')
+            upscaled=repeat_upsample(rgb,4, 4)
+            viewer.imshow(upscaled)
+
             replay_buffer.store_effect(obs_idx, action, reward, done)
 
             episode_rewards = get_wrapper_by_name(env, "Monitor").get_episode_rewards()
@@ -166,7 +186,7 @@ def main():
     env = get_env(task, seed)
     # session = get_session()
     # atari_learn(env, session, num_timesteps=task.max_timesteps)
-    test_model('GAME_model-215', env)
+    test_model('GAME_model-214', env)
 
 if __name__ == "__main__":
     main()
